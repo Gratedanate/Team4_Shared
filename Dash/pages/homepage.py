f456e26f8508b195d77e57f2f730d15ac24b61be
Gratedanate <ndbrewer@wm.edu>
2025-09-09 00:05:13 -0400

    runnable pageone

--- Dash/pages/homepage.py
+++ Dash/pages/homepage.py
@@ -6,4 +6,4 @@ dash.register_page(__name__, path="/")
 layout = html.Div([
     html.H2("US Census Data"),
     html.P("[Description of the Dashboard]")
-])
+])  
--- Dash/pages/pageone.py
+++ Dash/pages/pageone.py
@@ -1,9 +1,49 @@
-import dash
-from dash import html
+from dash import html, dcc, callback, register_page, Output, Input
+import pandas as pd
+import plotly.express as px
+from pathlib import Path
 
-dash.register_page(__name__, path="/pageone", name = "Page 1")
+register_page(__name__, path="/pageone", name = "Page 1")
+
+data = Path(__file__).resolve().parent.parent / "data" / "banks.csv"
+
+df = pd.read_csv(data, encoding='latin1')
+
+df.columns = df.columns.str.strip()
+df['upd_ClosingDate'] = pd.to_datetime(df["Closing Date"], format="%d-%b-%y")
+df["year"] = df["upd_ClosingDate"].dt.year
+closures = df.groupby(["year", "State"]).size().reset_index(name="closures")
+
 
 layout = html.Div([
-    html.H2("Welcome to my Page 1"),
-    html.P("This is a simple multipage Dash project example.")
+    html.H1("Bank Closure Data since 2000"),
+    dcc.Slider(
+        id="slider",
+        min=(closures["year"].min()),
+        max=(closures["year"].max()),
+        value=(closures["year"].min()),
+        marks={str(y): str(y) for y in sorted(closures["year"].unique())},
+        step=None,
+    ),
+    dcc.Graph(id="map"),
 ])
+
+@callback(
+    Output("map", "figure"),
+    Input("slider", "value")
+)
+
+def bank_map(selected_year):
+    d = closures[closures["year"] == selected_year]
+    fig = px.choropleth(
+        d, 
+        title = f"Bank Closures in the US - {selected_year}",
+        labels = {"color": "Number of Closures"},
+        locations = "State",
+        locationmode = "USA-states",
+        scope ="usa",
+        color = "closures",
+        color_continuous_scale = "greens",
+    )
+
+    return fig
